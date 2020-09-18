f39814e51b306aef7ee3defeef9bb7e826616e26
Stratis Dermanoutsos <stratis450@gmail.com>
2020-09-18 11:29:50 +0300

    Started Development

---
+++ Assets/Scripts/BotRandomChoice.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class BotRandomChoice : MonoBehaviour
+{
+
+    void Awake()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
