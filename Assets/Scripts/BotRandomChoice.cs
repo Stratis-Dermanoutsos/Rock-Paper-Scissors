e6debee209663a5f95ec093b75e4d7235ac6b523
Stratis Dermanoutsos <stratis450@gmail.com>
2020-09-18 12:27:47 +0300

    Added player playability

--- Assets/Scripts/BotRandomChoice.cs
+++ Assets/Scripts/BotRandomChoice.cs
@@ -1,16 +1,25 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class BotRandomChoice : MonoBehaviour
 {
+    [SerializeField] private Image myEmote;
+    private int randomChoice;
+    private GameManager gm;
 
     void Awake()
     {
-        
+        gm = GameObject.Find("GameManager").GetComponent<GameManager>();
+
+        // Select a Random Emote from the available choises
+        randomChoice = Random.Range(0, 3);
     }
 
-    // Update is called once per frame
-    void Update()
+    // Reveal and return bot's choice
+    public int Choice()
     {
-        
+        myEmote.sprite = gm.handEmotes[randomChoice];
+
+        return randomChoice;
     }
 }
---
+++ Assets/Scripts/PlayerChoice.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerChoice : MonoBehaviour
+{
+    [SerializeField] private Image myEmote;
+
+    public int playerChoice;
+    private GameManager gm;
+
+    void Awake()
+    {
+        gm = GameObject.Find("GameManager").GetComponent<GameManager>();
+    }
+
+    public void RockButton() 
+    {
+        myEmote.sprite = gm.handEmotes[0];
+
+        playerChoice = 0;
+    }
+
+    public void PaperButton()
+    {
+        myEmote.sprite = gm.handEmotes[1];
+
+        playerChoice = 1;
+    }
+
+    public void ScissorsButton()
+    {
+        myEmote.sprite = gm.handEmotes[2];
+
+        playerChoice = 2;
+    }
+}
